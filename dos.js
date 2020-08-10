e9cab159c616adfc4bebcd541f8e17a9678168bf
CarolinaYoko <68036337+CarolinaYoko@users.noreply.github.com>
2020-08-10 20:08:35 -0300

    terminado

--- dos.js
+++ dos.js
@@ -5,7 +5,72 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
-*/function mostrar()
-{
-  alert("dos");
+*/
+function mostrar() {
+
+  let marca;
+  let precio;
+  let peso;
+  let tipo;
+  let acumulador = 0;
+  let liqcaro;
+  let marcacara;
+  let banderamarca = 0;
+  let banderamarca2 = 0;
+  let marcabarata;
+  let solidobarato;
+
+  do {
+    marca = prompt("ingrese una marca");
+    while (!(isNaN(marca))) {
+      marca = prompt("Error. Ingrese una marca");
+    }
+    precio = parseInt(prompt("ingrese un precio"));
+    while (isNaN(precio) || precio < 0) {
+      precio = parseInt(prompt("Error. ingrese un precio"));
+    }
+    peso = parseInt(prompt("ingrese un peso en kilogramos"));
+    while (isNaN(peso) || peso < 0) {
+      peso = parseInt(prompt("Error. ingrese un peso"));
+    }
+    tipo = prompt("ingrese un tipo");
+    while (!(tipo == "solido" || tipo == "liquido")) {
+      tipo = prompt("Error. ingrese un tipo");
+    }
+
+    acumulador += peso;
+
+    switch (tipo) {
+      case "liquido":
+        if (banderamarca == 0 || liqcaro < precio) {
+          banderamarca = 1;
+          liqcaro = precio;
+          marcacara = marca;
+        }
+        break;
+
+      case "solido":
+        if (banderamarca2 == 0 || solidobarato > precio) {
+          banderamarca2 = 1;
+          solidobarato = precio;
+          marcabarata = marca;
+        }
+        break;
+
+    }
+
+    respuesta = prompt("¿desea seguir ingresando? s/n:");
+
+  } while (respuesta == "s");
+
+
+
+  console.log("el peso total de la compra es: " + acumulador);
+  console.log("la marca del más caro de los líquidos es: " + marcacara);
+  console.log("la marca del más barato de los sólidos es: " + marcabarata);
+
 }
+/*
+a)informar el peso total de la compra.
+b)la marca del más caro de los líquidos
+c)la marca del más barato de los sólidos*/
--- tres.js
+++ tres.js
@@ -9,7 +9,89 @@ a)el lugar más elegido
 b)el sexo de titular que lleva más pasajeros.
 c)el promedio de personas por viaje,  que viajan en invierno
 */
-function mostrar()
-{
-	alert("tres");
+function mostrar() {
+
+	let sexo;
+	let lugar;
+	let temporada;
+	let cantidad;
+	let contadorBariloche = 0;
+	let contadorCataratas = 0;
+	let contadorSalta = 0;
+	let elmaselegido;
+	let banderaTitular = 0;
+	let mayorcantidad;
+	let sexotitular;
+	let promedio;
+	let acumuladorInvierno = 0;
+	let contadorInvierno = 0;
+
+	do {
+		sexo = prompt("ingrese sexo (f o m)");
+		while (!(sexo == "f" || sexo == "m")) {
+			sexo = prompt("Error.Ingrese sexo");
+		}
+		lugar = prompt('ingrese lugar:“bariloche”, “cataratas” o “salta”)');
+		while (!(lugar == "bariloche" || lugar == "cataratas" || lugar == "salta")) {
+			lugar = prompt("Error.Ingrese lugar");
+		}
+		temporada = prompt('Ingrese temporada: “otoño”,”invierno, “verano,”primavera”');
+		while (!(temporada == "otoño" || temporada == "invierno" || temporada == "verano" || temporada == "primavera")) {
+			temporada = prompt("Error.Ingrese temporada");
+		}
+		cantidad = parseInt(prompt("Ingrese la cantidad de personas que viajan"));
+		while (isNaN(cantidad)) {
+			cantidad = parseInt(prompt("Error. Ingrese la cantidad de personas que viajan"));
+		}
+
+		switch (lugar) {
+			case "bariloche":
+				contadorBariloche++;
+				break;
+			case "cataratas":
+				contadorCataratas++;
+				break;
+			case "salta":
+				contadorSalta++;
+				break;
+		}
+
+		if (banderaTitular == 0 || mayorcantidad < cantidad) {
+			banderaTitular = 1;
+			mayorcantidad = cantidad;
+			sexotitular = sexo;
+
+		}
+
+		if (temporada == "invierno") {
+			acumuladorInvierno += cantidad;
+			contadorInvierno++;
+		}
+		respuesta = prompt("¿desea contituar ingresando? s/n")
+
+	} while (respuesta == "s"); //fin de iteración
+
+	if (contadorBariloche > contadorCataratas && contadorBariloche > contadorSalta) {
+		elmaselegido = "Bariloche";
+	}
+	else if (contadorCataratas > contadorSalta && contadorCataratas > contadorBariloche) {
+		elmaselegido = "Cataratas";
+	}
+	else if (contadorSalta > contadorBariloche && contadorSalta > contadorCataratas) {
+		elmaselegido = "Salta";
+	}
+	else {
+		elmaselegido = "hay mas de uno";
+	}
+
+	promedio = acumuladorInvierno / contadorInvierno;
+
+
+	console.log("El lugar mas elegido es: " + elmaselegido);
+	console.log("el sexo de titular que lleva más pasajeros es: " + sexotitular);
+	console.log("El promedio de personas por viaje,  que viajan en invierno es: " + promedio);
+
 }
+/*a)el lugar más elegido
+b)el sexo de titular que lleva más pasajeros.
+c)el promedio de personas por viaje,  que viajan en invierno*/
